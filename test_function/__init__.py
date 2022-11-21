f112411b9bf5a5547e9fb97fb7c91bbded2ca6cb
Tejesh Chauragade <chauragade.tejesh@gmail.com>
2022-11-21 10:59:57 +0530

    feat: test function for route variables etc...

---
+++ test_function/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    edu = req.route_params.get('edu')
+    name = req.params.get('name')
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        lastname = req_body.get('lastname')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name} {lastname} ({edu}). This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
